5723a1516515a59a0435aeee85913cd2da6392e0
shounakbhattacharjee <shounak097@gmail.com>
2023-02-24 04:37:41 +0530

    Adding delete and update feature.

--- src/main/java/com/example/ShoppingApp/service/CategoryService.java
+++ src/main/java/com/example/ShoppingApp/service/CategoryService.java
@@ -18,4 +18,12 @@ public class CategoryService {
     public void addCategory(Category category){
         categoryRepository.save(category);
     }
+
+    public void removeCategoryById(int id){
+        categoryRepository.deleteById(id);
+    }
+
+    public Optional<Category> getCategoryById(int id){
+        return categoryRepository.findById(id);
+    }
 }
